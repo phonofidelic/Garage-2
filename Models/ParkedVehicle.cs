f342dc7b964918f68c9e15b52f8369fdbb48ace2
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2025-12-17 10:53:13 +0100

    add ParkedVehicle model constraints

--- Models/ParkedVehicle.cs
+++ Models/ParkedVehicle.cs
@@ -5,13 +5,31 @@ namespace Garage_2.Models
     public class ParkedVehicle
     {
         public int Id { get; set; }
-        [StringLength(6)]
+
+        [Required]
+        [StringLength(6, MinimumLength = 6)]
         public string RegistrationNumber { get; set; } = default!;
+
+        [Required]
+        [StringLength(100)]
         public string Make { get; set; } = default!;
+
+        [Required]
+        [StringLength(100)]
         public string Model { get; set; } = default!;
+
+        [Required]
+        [Range(0, 22)]
         public int NumberOfWheels { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Color { get; set; } = default!;
-        public DateTime ArrivalTime { get; set; }
+
+        [Required]
+        public DateTime ArrivalTime { get; } = DateTime.Now;
+
+        [Required]
         public VehicleType Type { get; set; }
 
     }
