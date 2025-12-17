e25970b21de41b6ea05a7cae64cd491860f48f52
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2025-12-17 09:59:08 +0100

    add initial ParkedVehicle model

---
+++ Models/ParkedVehicle.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Garage_2.Models
+{
+    public class ParkedVehicle
+    {
+        public int Id { get; set; }
+        [StringLength(6)]
+        public string RegistrationNumber { get; set; } = default!;
+        public string Make { get; set; } = default!;
+        public string Model { get; set; } = default!;
+        public int NumberOfWheels { get; set; }
+        public string Color { get; set; } = default!;
+        public DateTime ArrivalTime { get; set; }
+        public VehicleType Type { get; set; }
+
+    }
+}
---
+++ Models/VehicleType.cs
@@ -0,0 +1,10 @@
+﻿namespace Garage_2.Models
+{
+    public enum VehicleType
+    {
+        Car,
+        Motercycle,
+        Boat,
+        Bus
+    }
+}
