205453ef0458f4c2b29e8fe2d99551c5df4d1c89
faal <130977909+faal2@users.noreply.github.com>
2025-12-17 19:53:41 +0100

    added overviewViewModel with id, type, rgNo, arrival time.

---
+++ Models/ViewModels/OverviewViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Garage_2.Models.ViewModels
+{
+    public class OverviewViewModel
+    {
+
+        public int Id { get; set; }
+
+        public VehicleType Type { get; set; }
+
+        public string RegistrationNUmber { get; set; }
+
+        public DateTime ArrivalTime { get; set; }
+
+    }
+}
