da2fc7ebad7dc1df80b0422a286002e3da25ed7d
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2025-12-18 11:46:34 +0100

    update default route to ParkedVehicles controller index

--- Program.cs
+++ Program.cs
@@ -36,7 +36,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=ParkedVehicles}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 DbInitializer.Seed(app);
