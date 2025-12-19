49967221518939529bb68cf9561a14f334621075
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2025-12-19 15:59:45 +0100

    move OverviewListItemViewModel to separate file

---
+++ Models/ViewModels/OverviewListItemViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Garage_2.Models.ViewModels
+{
+    public class OverviewListItemViewModel
+    {
+        public int Id { get; set; }
+
+        public VehicleType Type { get; set; }
+
+        [Display(Name = "Registration number")]
+        public string RegistrationNumber { get; set; } = default!;
+
+        [Display(Name = "Arrival time")]
+        public DateTime ArrivalTime { get; set; }
+
+        [Display(Name = "Parked time")]
+        public TimeSpan ParkedTime { get; set; }
+    }
+}
--- Models/ViewModels/OverviewViewModel.cs
+++ Models/ViewModels/OverviewViewModel.cs
@@ -12,20 +12,4 @@ namespace Garage_2.Models.ViewModels
 
         public int Count { get; set; }
     }
-
-    public class OverviewListItemViewModel
-    {
-        public int Id { get; set; }
-
-        public VehicleType Type { get; set; }
-
-        [Display(Name = "Registration number")]
-        public string RegistrationNumber { get; set; } = default!;
-
-        [Display(Name = "Arrival time")]
-        public DateTime ArrivalTime { get; set; }
-
-        [Display(Name = "Parked time")]
-        public TimeSpan ParkedTime { get; set; }
-    }
 }
