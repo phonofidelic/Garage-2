693b2bb7b3a69214f9a0af0bf78c3a11d30ffcbf
Martin Olsson <martinolsson89@gmail.com>
2025-12-29 16:19:47 +0100

    published to azure

--- Program.cs
+++ Program.cs
@@ -32,6 +32,12 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<GarageContext>();
+    db.Database.Migrate();
+}
+
 app.UseHttpsRedirection();
 app.UseRouting();
 
