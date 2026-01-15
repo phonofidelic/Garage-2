b1a651e1904198bcab725b8344f7a1b621fe43ad
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2026-01-15 11:02:31 +0100

    add VehicleType

---
+++ Models/Entities/VehicleType.cs
@@ -0,0 +1,11 @@
+﻿namespace Garage_2.Models.Entities
+{
+    public class VehicleType
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Size { get; set; }
+    }
+}
