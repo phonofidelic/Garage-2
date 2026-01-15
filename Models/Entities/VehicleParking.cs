2e18a0ce261e818c5e62e1ec05a4059856a534ac
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2026-01-15 11:38:44 +0100

    add VehicleParking and ParkingSession entities

---
+++ Models/Entities/VehicleParking.cs
@@ -0,0 +1,13 @@
+﻿namespace Garage_2.Models.Entities
+{
+    public class VehicleParking
+    {
+        public int ParkingSessionId { get; set; }
+        public ParkingSession ParkingSession { get; set; }
+
+        public int ParkingSpotId { get; set; }
+        public ParkingSpot ParkingSpot { get; set; }
+
+        public int UnitsUsed { get; set; }
+    }
+}
