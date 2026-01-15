2e015ad53f4fab848950eae8cc96a69e2eefc375
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2026-01-15 11:40:30 +0100

    add Vehicle entity

---
+++ Models/Entities/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿namespace Garage_2.Models.Entities
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+
+        public string RegistrationNumber { get; set; }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int NumberOfWheels { get; set; }
+
+        public int VehicleTypeId { get; set; }
+        public VehicleType Type { get; set; }
+
+        public int ApplicationUserId { get; set; }
+        public ApplicationUser User { get; set; }
+
+        public ICollection<ParkingSession> ParkingSessions{ get; set; }
+    }
+}
