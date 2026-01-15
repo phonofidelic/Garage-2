37a1b912a27c83fa5960b7a9f7ed4b4d6b3a963a
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2026-01-15 09:02:14 +0100

    add ApplicationUser entity

---
+++ Models/Entities/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Garage_2.Models.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string SSN { get; set; }
+    }
+}
