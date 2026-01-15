9af48aacdffd4c0ffd1b2c7648a4b5df899aaf6e
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2026-01-15 11:31:30 +0100

    add ParkingSpot entity

---
+++ Models/Entities/ParkingSpot.cs
@@ -0,0 +1,13 @@
+﻿namespace Garage_2.Models.Entities
+{
+    public class ParkingSpot
+    {
+        public int Id { get; set; }
+
+        public int SpotNumber { get; set; }
+
+        public int CapacityUnits { get; set; }
+
+        public bool IsBlocked { get; set; }
+    }
+}
