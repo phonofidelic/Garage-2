e2ab95bbe1283782f32cd9cf6b43a4a573001783
Christopher Clemons <christopher@MacBookPro.lan>
2026-01-17 01:15:03 +0100

    add PageTitle in ParkingSessionsIndexViewModel

--- Models/ViewModels/ParkingSessions/ParkingSessionsIndexViewModel.cs
+++ Models/ViewModels/ParkingSessions/ParkingSessionsIndexViewModel.cs
@@ -5,6 +5,7 @@ namespace Garage_2.Models.ViewModels.ParkingSessions
 {
     public class ParkingSessionsIndexViewModel
     {
+        public string PageTitle { get => "Parking History"; }
         public ParkingSessionsListParameters ListParameters { get; set; } = default!;
         public List<ParkingSessionsListItemViewModel> ParkingSessionsList { get; set; } = [];
     }
